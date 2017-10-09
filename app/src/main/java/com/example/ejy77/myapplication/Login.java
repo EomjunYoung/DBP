187dfd96553a108e5759600878d2a2ae75a74c6f
EomjunYoung <theoneday737@naver.com>
2017-10-09 16:44:53 +0900

    add intent(login, register)

--- app/src/main/java/com/example/ejy77/myapplication/Login.java
+++ app/src/main/java/com/example/ejy77/myapplication/Login.java
@@ -1,8 +1,10 @@
 package com.example.ejy77.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -25,6 +27,17 @@ public class Login extends AppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn1 = (Button)findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, MemberRegister.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
