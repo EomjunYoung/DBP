5e5825aef95e658c247ac30c3017ece0479a9d27
EomjunYoung <theoneday737@naver.com>
2017-10-14 22:14:30 +0900

    add the adapter

---
+++ app/src/main/java/com/example/ejy77/myapplication/Mainitembar.java
@@ -0,0 +1,63 @@
+package com.example.ejy77.myapplication;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by ejy77 on 2017-10-14.
+ */
+
+public class Mainitembar
+{
+
+
+    private Drawable itemimage;
+    private String itemname;
+    private String itemprice;
+    private String itempoint;
+    private String itemexplain;
+
+
+    public Drawable getItemimage() {
+        return itemimage;
+    }
+
+    public void setItemimage(Drawable itemimage) {
+        this.itemimage = itemimage;
+    }
+
+    public String getItemname() {
+        return itemname;
+    }
+
+    public void setItemname(String itemname) {
+        this.itemname = itemname;
+    }
+
+    public String getItemprice() {
+        return itemprice;
+    }
+
+    public void setItemprice(String itemprice) {
+        this.itemprice = itemprice;
+    }
+
+    public String getItempoint() {
+        return itempoint;
+    }
+
+    public void setItempoint(String itempoint) {
+        this.itempoint = itempoint;
+    }
+
+    public String getItemexplain() {
+        return itemexplain;
+    }
+
+    public void setItemexplain(String itemexplain) {
+        this.itemexplain = itemexplain;
+    }
+
+
+
+
+}
