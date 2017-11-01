f6cba0506be17515e09fddb0c1784cb17454a8b3
EomjunYoung <theoneday737@naver.com>
2017-11-01 19:06:22 +0900

    add main app

--- app/src/main/java/com/example/ejy77/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ejy77/myapplication/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.ejy77.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btn1, btn2, btn3, btn4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_app);
+
+
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+        btn4 = (Button)findViewById(R.id.btn4);
+
     }
 }
